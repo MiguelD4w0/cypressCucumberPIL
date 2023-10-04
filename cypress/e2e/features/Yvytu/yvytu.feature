335b78a4adddcb9993a299ccec2296abe04adbf2
MiguelD4w0 <miguel.lopez@darwoft.com>
2023-10-03 22:36:59 -0300

    Se agregaron ejemplos de tablas

--- cypress/e2e/features/Yvytu/yvytu.feature
+++ cypress/e2e/features/Yvytu/yvytu.feature
@@ -4,3 +4,25 @@ Feature: Home Yvytu
     Scenario: Verificar elementos del menu superior
         Given que un usuario esta en la pagina de "Yvytu"
         Then visualiza en el header los botones "LA RESERVA, CABAÑAS, COMO LLEGAR, CONTACTO, DONÁ"
+
+    
+    Scenario: Verificar Imagenes del banner Principal
+        Given que un usuario esta en la pagina de "Yvytu"
+        Then se visualizan las imagenes "01, 02, 03, 04" en el banner
+
+    
+    Scenario: Verificar comportamiento del boton Ir arriba 
+        Given que un usuario esta en la pagina de "Yvytu"
+        Then el boton "Ir arriba" no se visualiza
+        When el usuario hace scroll hasta "Conocé una historia mágica."
+        Then el boton "Ir arriba" se visualiza
+        When el usuario presiona el boton "Ir arriba"
+        Then visualiza en el header los botones "LA RESERVA, CABAÑAS, COMO LLEGAR, CONTACTO, DONÁ"
+        And el boton "Ir arriba" no se visualiza
+
+    @focus
+    Scenario: Verificar comportamiento del botón /reserva_yvytu
+        Given que un usuario esta en la pagina de "Yvytu"
+        Then visualiza el link "Reservar" redirecciona a "https://wa.me/5493757454400"
+        And visualiza el link "reserva_yvytu" redirecciona a "https://www.instagram.com/reserva_yvytu/"
+        And visualiza el link "Enviar mensaje" redirecciona a "https://wa.me/5493757454400"
--- cypress/e2e/step_definitions/yvytu.js
+++ cypress/e2e/step_definitions/yvytu.js
@@ -1,5 +1,13 @@
-import { Then } from "@badeball/cypress-cucumber-preprocessor";
+import { When, Then } from "@badeball/cypress-cucumber-preprocessor";
 const yvytuHome = require("../../pages/yvytupage");
+
+When (`el usuario hace scroll hasta {string}`, (scrollToTxt) => {
+    cy.contains(scrollToTxt).scrollIntoView();
+});
+
+When (`el usuario presiona el boton {string}`, (btnName) => {
+    yvytuHome.getGenericBtn().contains(btnName).click();
+});
 
 Then(`visualiza en el header los botones {string}`, (list) => {
     list = list.split(", ");
@@ -11,3 +19,40 @@ Then(`visualiza en el header los botones {string}`, (list) => {
     });
 });
 
+Then (`se visualizan las imagenes {string} en el banner`, (imgList) => {
+    imgList = imgList.split(", ");
+
+    imgList.forEach((banner, inx) => {
+        yvytuHome
+          .getCurrentImageBanner()
+          .should(
+            "have.class",
+            `bg-[url('/public/images/header-gallery/${banner}.png')]`
+        );
+    
+        yvytuHome
+          .getImgButton()
+          .its("length")
+          .then((cantidad) => {
+            if (cantidad != inx + 1) {
+              yvytuHome
+                .getImgButton()
+                .eq(inx + 1)
+                .click();
+              cy.wait(1000);
+            }
+        });
+    });
+});
+
+Then (`el boton {string} no se visualiza`, (btnName) => {
+    yvytuHome.getGenericBtn().contains(btnName).should("not.be.visible");
+});
+
+Then (`el boton {string} se visualiza`, (btnName) => {
+    yvytuHome.getGenericBtn().contains(btnName).should("be.visible");
+});
+
+Then (`visualiza el link {string} redirecciona a {string}`, (btnName, link) => {
+    yvytuHome.getGenericLink().contains(btnName).should("have.attr", "href", link)
+});
