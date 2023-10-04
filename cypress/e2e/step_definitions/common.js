6429eca11e3f434a25eba608d20dda6d8927c8f4
MiguelD4w0 <miguel.lopez@darwoft.com>
2023-10-04 03:58:00 -0300

    Se mejora url

--- cypress/e2e/step_definitions/common.js
+++ cypress/e2e/step_definitions/common.js
@@ -1,10 +1,13 @@
 import { Given } from "@badeball/cypress-cucumber-preprocessor";
 
 Given(`que un usuario esta en la pagina de {string}`, (urlName ) => {
+    /*const url = Cypress.env("YVYTY_URL")
+    cy.log(url)
     if(urlName == "Yvytu"){
         cy.visit("https://vientosdelaselva.com.ar/")
     }else if(urlName == "Eden"){
         cy.visit("https://www.edenentradas.com.ar/")
-    }
-    
+    }*/
+    const url = Cypress.env(`${urlName.toUpperCase()}_URL`);
+    cy.visit(url);
 });
